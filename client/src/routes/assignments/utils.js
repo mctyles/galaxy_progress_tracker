9458000f70521e771fd34a2a2850e51d101034e8
tylermcmu <94275794+tylermcmu@users.noreply.github.com>
2023-07-07 15:15:14 -0700

    Configured scripts for deployment.

--- client/src/routes/assignments/utils.js
+++ client/src/routes/assignments/utils.js
@@ -22,35 +22,3 @@ export function filterAssignments(searchQuery, assignments) {
 
   return filteredAssignments;
 }
-
-export const handleFormSubmission = async (event) => {
-  event.preventDefault();
-
-  const assignment = await addNewAssignment(user?.token, {
-    name,
-    totalPoints,
-    categoryId,
-    dateAssigned: dateAssigned.startDate,
-    teacherId: user?.user.id,
-  });
-
-  if (assignment) {
-    setSubmitSuccess(true);
-    navigate(0);
-  }
-};
-
-export const handleNameChanged = (event) => {
-  const enteredName = event.target.value;
-  setName(enteredName);
-};
-
-export const handleTotalPointsChanged = (event) => {
-  const enteredTotalPoints = event.target.value;
-  setTotalPoints(enteredTotalPoints);
-};
-
-export const handleCategoryChanged = (event) => {
-  const selectedCategoryId = event.target.value;
-  setCategoryId(selectedCategoryId);
-};
--- server/src/db/client.js
+++ server/src/db/client.js
@@ -5,6 +5,11 @@ const DB_NAME = "galaxy-progress-tracker";
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
-const client = new Client(DB_URL);
+const client = new Client({
+  connectionString: DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = client;
